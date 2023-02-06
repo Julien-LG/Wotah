9bab4b1840e2677560586b2672a10f102ea9bbcc
Julien LG <58564546+Julien-LG@users.noreply.github.com>
2023-02-06 14:49:00 +0100

    ajout classe modele

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,2 +1,14 @@
 public class Personnage {
+    private final int numero;
+    private int x, y;
+    private int vie;
+    private boolean vivant;
+
+    public Personnage(int numero, int x, int y){
+        this.numero = numero;
+        this.x = x;
+        this.y = y;
+        this.vie = ;
+    }
+
 }
